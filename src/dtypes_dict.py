7f8611c7a95e63291666a764d1c362f9f3e6106f
crahal <charlierahal@gmail.com>
2021-08-10 13:47:49 +0100

    first commit, basic dtypes file

---
+++ src/dtypes_dict.py
@@ -0,0 +1,14 @@
+dtypes_dict = {'dc:identifier': str,
+               'dc:title': str,
+               'prism:aggregationType': str,
+               'subtypeDescription': str,
+               'citedby-count': int,
+               'prism:publicationName': str,
+               'prism:coverDate': str,
+               'prism:doi': str,
+               'openaccess': int,
+               'author': str,
+               'dc:description': str,
+               'authkeywords': str,
+               'subj': str
+               }
